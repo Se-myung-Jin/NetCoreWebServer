02595a532a1615bc2ef2b965221593e147ebeb7e
Se-myung-Jin <tpaud94@naver.com>
2024-11-03 23:06:02 +0900

    Add Program file

---
+++ NetCoreWebServer/001. NetCoreWebServer/NetCoreWebServer/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace NetCoreWebServer
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateWebHostBuilder(args).Build().RunAsync();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls(Config.Instance.ServerUrl);
+    }
+}
