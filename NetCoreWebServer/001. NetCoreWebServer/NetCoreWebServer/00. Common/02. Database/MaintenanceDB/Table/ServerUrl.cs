85fdaa1c0a779fddbb797971d5740e283a6c432d
Se-myung-Jin <tpaud94@naver.com>
2024-11-13 00:37:38 +0900

    MySql, Dapper 추가

---
+++ NetCoreWebServer/001. NetCoreWebServer/NetCoreWebServer/00. Common/02. Database/MaintenanceDB/Table/ServerUrl.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using MySqlConnector;
+
+namespace NetCoreWebServer
+{
+    public class ServerUrl
+    {
+        public int Seq { get; private set; }
+        public string Name { get; private set; }
+        public string Version { get; private set; }
+        public string Url { get; private set; }
+
+        public static async Task<List<ServerUrl>> FetchAllAsync()
+        {
+            var sql = "select `seq`, `name`, `version`, `url` from `serverUrl`;";
+
+            await using (var conn = new MySqlConnection(Config.Instance.MaintenanceDB))
+            {
+                return (await conn.QueryAsync<ServerUrl>(sql)).AsList();
+            }
+        }
+    }
+}
