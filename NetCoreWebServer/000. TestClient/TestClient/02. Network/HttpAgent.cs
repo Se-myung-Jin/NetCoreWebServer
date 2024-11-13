b2d22139091eb72959dcea45e1bbfaff4f745762
Se-myung-Jin <tpaud94@naver.com>
2024-11-13 23:47:31 +0900

    테스트 클라이언트 용 Http 코드 추가

---
+++ NetCoreWebServer/000. TestClient/TestClient/02. Network/HttpAgent.cs
@@ -0,0 +1,36 @@
+﻿using MessagePack;
+using WebProtocol;
+
+namespace TestClient
+{
+    public class HttpAgent
+    {
+        private static Http http = new Http(TimeSpan.FromSeconds(7));
+
+        public static async Task<T> RequestLobbyAsync<T>(string lobbyUrl, Protocol protocol) where T : ProtocolRes, new()
+        {
+            return await RequestAsync<T>(lobbyUrl, protocol);
+        }
+
+        private static async Task<T> RequestAsync<T>(string url, Protocol protocol) where T : ProtocolRes, new()
+        {
+            try
+            {
+                if (null == url || 0 >= url.Length)
+                {
+                    return new T { Result = Result.UnknownProtocol };
+                }
+
+                var packet = new ProtocolReq { Protocol = protocol };
+                var bytes = MessagePackSerializer.Serialize(packet);
+                var result = await http.RequestAsync(HttpMethod.Post, url, body: bytes);
+
+                return (T)MessagePackSerializer.Deserialize<ProtocolRes>(result.Data);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ NetCoreWebServer/000. TestClient/TestClient/02. Network/Response.cs
@@ -0,0 +1,72 @@
+﻿using System.Net;
+using System.Text;
+
+namespace TestClient
+{
+    public class Response
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public byte[] Data { get; set; }
+
+        public string ResponseString
+        {
+            get
+            {
+                try
+                {
+                    return Encoding.UTF8.GetString(Data);
+                }
+                catch (Exception ex)
+                {
+                }
+
+                return default;
+            }
+        }
+
+        public Response() { }
+
+        public Response(HttpStatusCode statusCode, byte[] response)
+        {
+            StatusCode = statusCode;
+            Data = response;
+        }
+
+        public Response(Response result)
+        {
+            StatusCode = result.StatusCode;
+            Data = result.Data;
+        }
+
+        public virtual void Set(HttpStatusCode statusCode, byte[] response)
+        {
+            StatusCode = statusCode;
+            Data = response;
+        }
+
+        public virtual void Set(Response result)
+        {
+            StatusCode = result.StatusCode;
+            Data = result.Data;
+        }
+
+        public T ToObject<T>() where T : Response
+        {
+            try
+            {
+                string json = Encoding.UTF8.GetString(Data);
+
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return default;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("StatusCode={0}, Response={1}", StatusCode, ResponseString);
+        }
+    }
+}
