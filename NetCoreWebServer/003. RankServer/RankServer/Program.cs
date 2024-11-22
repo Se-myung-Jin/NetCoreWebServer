ee2c4e7c21607c8e7553d40abfcac1c68ac400f5
Se-myung-Jin <tpaud94@naver.com>
2024-11-23 01:57:58 +0900

    3번째 프로젝트 랭크서버 추가

---
+++ NetCoreWebServer/003. RankServer/RankServer/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore;
+
+namespace RankServer
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateWebHostBuilder(args).Build().RunAsync();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("http://*:9000");
+    }
+}
---
+++ NetCoreWebServer/003. RankServer/RankServer/Startup.cs
@@ -0,0 +1,23 @@
+﻿namespace RankServer
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            
+        }
+    }
+}
